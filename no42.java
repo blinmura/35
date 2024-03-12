dd535828f518d2599bbbd16e9dea9d4922a954b6
blinmura <148384609+blinmura@users.noreply.github.com>
2024-03-13 02:07:15 +0600

    Update no42.java

--- no42.java
+++ no42.java
@@ -1,6 +1,6 @@
 public class no42 {
     public static void main(String[] args){
-        int age = 18; // replace with actual age
+        int age = 18; 
 if(age >= 18) {
     System.out.println("Rights can be obtained");
 } else {
