ce27caf8fc72ec1355ab93834117e8da3f3e8d56
blinmura <148384609+blinmura@users.noreply.github.com>
2024-03-13 02:07:29 +0600

    Update no62.java

--- no62.java
+++ no62.java
@@ -1,7 +1,7 @@
 public class no62 {
     public static void main(String[] args){
 
-        double radius = 5; // replace with actual radius
+        double radius = 5; 
 double volume = 4.0/3.0 * 3.14 * Math.pow(radius, 3);
 System.out.println(volume);
 
