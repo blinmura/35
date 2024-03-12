dcb84224f3e64e6d95810ebfafc347898bec16d6
blinmura <148384609+blinmura@users.noreply.github.com>
2024-03-13 02:06:26 +0600

    Update arrayno22.java

--- arrayno22.java
+++ arrayno22.java
@@ -1,6 +1,6 @@
 public class arrayno22 {
     public static void main(String[] args){
-        int num = 5; // number for which to print the multiplication table
+        int num = 5; 
 for (int i = 1; i <= 10; i++) {
     System.out.println(num + " * " + i + " = " + num * i);
 }
