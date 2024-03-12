f50c73093479e0eec4b591917870d76eab3ff49a
blinmura <148384609+blinmura@users.noreply.github.com>
2024-03-13 02:06:08 +0600

    Update arrayno12.java

--- arrayno12.java
+++ arrayno12.java
@@ -1,7 +1,7 @@
 public class arrayno12 {
     public static void main(String[] args){
         int[] array = {1, 2, 3, 4, 5};
-int num = 3; // number to check
+int num = 3; 
 boolean exists = false;
 for (int i : array) {
     if (i == num) {
